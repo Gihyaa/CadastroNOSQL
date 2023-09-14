df50e9bff577be76f8d4fa7ad61ee37dab788f64
Gihyaa <giovannaap.vivencio@gmail.com>
2023-09-14 14:58:13 -0300

    AppNOSQL

---
+++ InterfaceApp/app/src/main/java/com/example/interfaceapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.interfaceapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val coralColor = Color(0xFFFFA07A)
+val LightBlueColor = Color(0xFFE0E0FF)
+val PurpleColor = Color(0xFF8563C2)
+
+val RedColor = Color(0xFF8563C2)
+val orangeredColor = Color(0xFF8563C2)
+val orangeColor = Color(0xFFFFA500)
